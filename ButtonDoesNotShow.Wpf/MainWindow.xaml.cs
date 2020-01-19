9da75fbc7abb707a4d1f8ab5b83e46ed299d0353
pauldendulk <pauldendulk@gmail.com>
2020-01-19 22:31:11 +0100

    Add complete sample

---
+++ ButtonDoesNotShow.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms.Platform.WPF;
+using Xamarin.Forms;
+
+namespace ButtonDoesNotShow.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : FormsApplicationPage
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Xamarin.Forms.Forms.Init();
+
+            LoadApplication(new ButtonDoesNotShow.App());
+        }
+    }
+}
---
+++ ButtonDoesNotShow/ButtonDoesNotShow/MyCustomView.cs
@@ -0,0 +1,18 @@
+﻿using SkiaSharp;
+using SkiaSharp.Views.Forms;
+
+namespace ButtonDoesNotShow
+{
+    public class MyCustomView : SKGLView
+    {
+        public MyCustomView()
+        {
+            PaintSurface += MyCustomView_PaintSurface;
+        }
+
+        private void MyCustomView_PaintSurface(object sender, SKPaintGLSurfaceEventArgs e)
+        {
+            e.Surface.Canvas.Clear(new SKColor(255, 128, 128));
+        }
+    }
+}
